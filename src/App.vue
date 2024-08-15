11a13c9eac347904ae4da493fe89561a5a0b6aa8
lucky <wangfuc@133.cn>
2024-08-15 09:08:25 +0800

    lucky commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<el-config-provider><!-- 使用 ElConfigProvider 包装您的根组件。(见ElementPlus) -->
+	<el-config-provider><!-- 自定义命名空间：使用 ElConfigProvider 包装您的根组件。(见ElementPlus) -->
 		这是首页，入口页
 		<router-view /><!-- 意思是，当要访问地址与路由<router-link>中的path一致时，将<router-view />替换成对应路由path对应的组件显示 -->
 	</el-config-provider>
